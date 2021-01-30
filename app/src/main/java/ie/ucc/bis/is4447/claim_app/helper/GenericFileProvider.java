0bce7832e209ff2fc1c131b8a616f602172f89b6
anthonymcsweeney1 <117380531@umail.ucc.ie>
2021-01-30 18:59:29 +0000

    Invoice PDF

---
+++ app/src/main/java/ie/ucc/bis/is4447/claim_app/helper/GenericFileProvider.java
@@ -0,0 +1,7 @@
+package ie.ucc.bis.is4447.claim_app.helper;
+
+
+import androidx.core.content.FileProvider;
+
+public class GenericFileProvider extends FileProvider {
+}
--- app/src/main/java/ie/ucc/bis/is4447/claim_app/view/InvoicePage.java
+++ app/src/main/java/ie/ucc/bis/is4447/claim_app/view/InvoicePage.java
@@ -1,17 +1,33 @@
 package ie.ucc.bis.is4447.claim_app.view;
-
+// https://stackoverflow.com/questions/55816746/filedownloader-at-the-android-it-is-showing-no-errors-but-the-download-does-not
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.FileProvider;
 
+import android.content.ActivityNotFoundException;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import java.io.File;
+import java.io.IOException;
+
+import ie.ucc.bis.is4447.claim_app.BuildConfig;
 import ie.ucc.bis.is4447.claim_app.R;
+import ie.ucc.bis.is4447.claim_app.helper.FileDownloader;
 
 public class InvoicePage extends AppCompatActivity {
 
@@ -20,11 +36,28 @@ public class InvoicePage extends AppCompatActivity {
     private BottomNavigationView bottomnav;
     private static final String TAG = "MyActivity";
 
+    private static final String[] PERMISSIONS = {android.Manifest.permission.READ_EXTERNAL_STORAGE, android.Manifest.permission.WRITE_EXTERNAL_STORAGE};
+
+
+    private static boolean hasPermissions(Context context, String... permissions) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && context != null && permissions != null) {
+            for (String permission : permissions) {
+                if (ActivityCompat.checkSelfPermission(context, permission) != PackageManager.PERMISSION_GRANTED) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_invoice_page);
+
+        Log.v(TAG, "onCreate() Method invoked ");
+
+        ActivityCompat.requestPermissions(InvoicePage.this, PERMISSIONS, 112);
 
 
         // Bottom Navigation
@@ -116,5 +149,100 @@ public class InvoicePage extends AppCompatActivity {
 
     }
 
+    public void request(View view) {
 
+        ActivityCompat.requestPermissions(InvoicePage.this, PERMISSIONS, 112);
+
+    }
+
+    public void view(View view) {
+        Log.v(TAG, "view() Method invoked ");
+
+        if (!hasPermissions(InvoicePage.this, PERMISSIONS)) {
+
+            Log.v(TAG, "download() Method DON'T HAVE PERMISSIONS ");
+
+            Toast t = Toast.makeText(getApplicationContext(), "You don't have read access !", Toast.LENGTH_LONG);
+            t.show();
+
+        } else {
+            File d = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);  // -> filename = maven.pdf
+            File pdfFile = new File(d, "Claim "+ClaimNum+".pdf");
+
+            Log.v(TAG, "view() Method pdfFile " + pdfFile.getAbsolutePath());
+
+            Uri path = FileProvider.getUriForFile(this, BuildConfig.APPLICATION_ID + ".fileprovider", pdfFile);
+
+
+            Log.v(TAG, "view() Method path " + path);
+
+            Intent pdfIntent = new Intent(Intent.ACTION_VIEW);
+            pdfIntent.setDataAndType(path, "application/pdf");
+            pdfIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            pdfIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+
+            try {
+                startActivity(pdfIntent);
+            } catch (ActivityNotFoundException e) {
+                Toast.makeText(InvoicePage.this, "No Application available to view PDF", Toast.LENGTH_SHORT).show();
+            }
+        }
+        Log.v(TAG, "view() Method completed ");
+
+    }
+
+    public void download(View view) {
+        Log.v(TAG, "download() Method invoked ");
+
+        if (!hasPermissions(InvoicePage.this, PERMISSIONS)) {
+
+            Log.v(TAG, "download() Method DON'T HAVE PERMISSIONS ");
+
+            Toast t = Toast.makeText(getApplicationContext(), "You don't have write access !", Toast.LENGTH_LONG);
+            t.show();
+
+        } else {
+            Log.v(TAG, "download() Method HAVE PERMISSIONS ");
+
+            //new DownloadFile().execute("http://maven.apache.org/maven-1.x/maven.pdf", "maven.pdf");
+            new DownloadFile().execute("https://vendorcentral.000webhostapp.com/downloads.php?file_id="+InvoiceNum, "Claim "+ClaimNum+".pdf");
+
+        }
+
+        Log.v(TAG, "download() Method completed ");
+
+    }
+
+    private class DownloadFile extends AsyncTask<String, Void, Void> {
+
+        @Override
+        protected Void doInBackground(String... strings) {
+            Log.v(TAG, "doInBackground() Method invoked ");
+
+            String fileUrl = strings[0];   // -> http://maven.apache.org/maven-1.x/maven.pdf
+            String fileName = strings[1];  // -> maven.pdf
+            String extStorageDirectory = Environment.getExternalStorageDirectory().toString();
+            File folder = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+
+            File pdfFile = new File(folder, fileName);
+            Log.v(TAG, "doInBackground() pdfFile invoked " + pdfFile.getAbsolutePath());
+            Log.v(TAG, "doInBackground() pdfFile invoked " + pdfFile.getAbsoluteFile());
+
+            try {
+                pdfFile.createNewFile();
+                Log.v(TAG, "doInBackground() file created" + pdfFile);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+                Log.e(TAG, "doInBackground() error" + e.getMessage());
+                Log.e(TAG, "doInBackground() error" + e.getStackTrace());
+
+
+            }
+            FileDownloader.downloadFile(fileUrl, pdfFile);
+            Log.v(TAG, "doInBackground() file download completed");
+
+            return null;
+        }
+    }
 }
